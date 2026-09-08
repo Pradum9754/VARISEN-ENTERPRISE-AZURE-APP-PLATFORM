0cc129f8de54e61368a6615d19f8fdbedb8ca3a5
Pradum <pradumprajapati482@gmail.com>
2026-09-08 23:52:06 +0530

    Delete Modules directory

--- Modules/azurerm_nat_gateway/output.tf
+++
@@ -1,7 +0,0 @@
-output "nat_gateway_ids" {
-  value = { for k, nat in azurerm_nat_gateway.preprod-nat-gateways : k => nat.id }
-}
-
-output "nat_gateway_names" {
-  value = { for k, nat in azurerm_nat_gateway.preprod-nat-gateways : k => nat.name }
-}
--- Modules/azurerm_network_security_group/output.tf
+++
@@ -1,7 +0,0 @@
-output "network_security_group_ids" {
-  value = { for k, nsg in azurerm_network_security_group.preprod-nsgs : k => nsg.id }
-}
-
-output "network_security_group_names" {
-  value = { for k, nsg in azurerm_network_security_group.preprod-nsgs : k => nsg.name }
-}
