ef3a019cb1e735865880ec4e36082a029ca87bc6
Pradum <pradumprajapati482@gmail.com>
2026-09-09 00:02:08 +0530

    Delete Modules directory

--- Modules/azurerm_bastion_host/output.tf
+++
@@ -1,9 +0,0 @@
-output "bastion_ids" {
-  value = { for k, bastion in azurerm_bastion_host.preprod_bastions : k => bastion.id
-  }
-}
-
-output "bastion_names" {
-  value = { for k, bastion in azurerm_bastion_host.preprod_bastions : k => bastion.name
-  }
-}
--- Modules/azurerm_resource_groups/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "preprod-rgs" {
-  type = map(object({
-    name     = string
-    location = string
-    tags     = optional(map(string))
-    managed_by = optional(string)
-  }))
-}
